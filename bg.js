86b018d3d2d0a53808dbc33526af80e41a66e4f6
Sagar Shinde <saagarshinde19@gmail.com>
2023-02-16 21:46:40 +0530

    Update bg.js
    Changed context menu location

--- bg.js
+++ bg.js
@@ -3,33 +3,21 @@
 chrome.contextMenus.create({
     id: "copy_setting_path",
     title: "Copy Setting Path",
-    contexts: ["page"]
+    contexts: ["all"]
 });
 
 chrome.contextMenus.create({
     id: "copy_hypertext_path",
     title: "Copy Hypertext Path",
-    contexts: ["page"]
+    contexts: ["all"]
 });
 
 chrome.contextMenus.create({
     id: "copy_setting_URL",
     title: "Copy Plain URL",
-    contexts: ["page"]
+    contexts: ["all"]
 });
-
-chrome.contextMenus.create({
-    id: "start_to_inspect",
-    title: "Start to Copy",
-    contexts: ["page"]
-});
-
-chrome.contextMenus.create({
-    id: "stop_inspect",
-    title: "Stop Copying",
-    contexts: ["page"]
-});
-
+				
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
     if (tab) {
         if ( [ "copy_setting_path", "copy_hypertext_path", "copy_setting_URL", "start_to_inspect", "stop_inspect" ].includes( info.menuItemId ) ){
